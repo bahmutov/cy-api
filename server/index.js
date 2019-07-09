44aaaed676d8255716c796baa38806d0f043d706
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-08 22:12:32 -0400

    start test server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// test server
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
