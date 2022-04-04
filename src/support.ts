7b519a7505763e50c17d865f21149e095dc7a04a
hang-up <robhang.up@gmail.com>
2022-04-04 16:12:47 -0400

    fix: Auth password return object.

--- src/support.ts
+++ src/support.ts
@@ -292,12 +292,15 @@ const formatJSon = (jsonObject: object) => {
 
 const formatRequest = (options: Partial<Cypress.RequestOptions>) => {
   const showCredentials = Cypress.env('API_SHOW_CREDENTIALS');
-  if (!showCredentials) {
-    let auth = options?.auth as { username?: string, password?: string };
-    if (auth?.password) {
-      auth.password = '*****';
-      options.auth = auth;
-    }
+  const hasPassword = options?.auth?.password;
+  if (!showCredentials && hasPassword) {
+    return formatJSon({
+      ...options,
+      auth: {
+        ...options.auth,
+        password: '*****'
+      }
+    })
   }
   return formatJSon(options);
 }
