3e28db0d91fb95cad2e302d70e527c9036fe5421
Nicolas AUBIN <dev@aubin.cc>
2023-03-24 13:40:24 +0100

    Create hides-non-requestoptions.cy.ts

---
+++ cypress/e2e/hides-non-requestoptions.cy.ts
@@ -0,0 +1,25 @@
+// loads definition for the custom "cy.api" command
+/// <reference path="../../dist/types.d.ts" />
+
+describe('hides-failOnStatusCode', () => {
+
+
+  it("hides non RequestOptions if env var `API_SHOW_GENUINE_REQUEST_OPTIONS_ONLY` is set to true", {
+    env: {
+      API_SHOW_GENUINE_REQUEST_OPTIONS_ONLY: true,
+    },
+  }, () => {
+    cy.api({
+      url: '/unknown-route',
+      log: false,
+      failOnStatusCode: false,
+      retryOnStatusCodeFailure: false,
+      retryOnNetworkFailure: false,
+    })
+    cy.get('.cy-api > div > .hljs')
+      .should("not.contain.text", `"log": false`)
+      .should("not.contain.text", `"failOnStatusCode": false`)
+      .should("not.contain.text", `"failOnStatusCode": false`)
+      .should("not.contain.text", `"failOnStatusCode": false`)
+  })
+})
