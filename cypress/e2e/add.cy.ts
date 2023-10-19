89be197e3dfe89d3cd272bce6a123b3e66ae5db0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-19 09:03:14 -0400

    add labels [skip ci]

--- cypress/e2e/add.cy.ts
+++ cypress/e2e/add.cy.ts
@@ -6,25 +6,34 @@ it('calls API methods', { viewportHeight: 2000 }, () => {
   // get the second random number from the server
   // call server to compute the sum
   // confirm the sum is correct
-  cy.api({
-    url: '/random-number',
-  })
+  cy.api(
+    {
+      url: '/random-number',
+    },
+    'first number',
+  )
     .its('body.n')
     .should('be.within', 0, 10)
     .then((a) => {
-      cy.api({
-        url: '/random-number',
-      })
+      cy.api(
+        {
+          url: '/random-number',
+        },
+        'second number',
+      )
         .its('body.n')
         .should('be.within', 0, 10)
         .then((b) => {
-          cy.api({
-            url: '/sum',
-            body: {
-              a,
-              b,
+          cy.api(
+            {
+              url: '/sum',
+              body: {
+                a,
+                b,
+              },
             },
-          })
+            'sum',
+          )
             .its('body.sum')
             .should('equal', a + b)
         })
